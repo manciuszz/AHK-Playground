233513e960fb6e069cb1108e950e310339544b3d
Mantas Murauskas <evilquaint@gmail.com>
2019-06-26 16:56:14 +0300

    ... undoed the JS scope changes, because I forgot about the 'handleClick' inside HTML

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,21 @@
-(function() {
-	let handleClick = function() {
-		let input = edit.getValue();
-		let output = document.getElementById('output');
-		
-		output.textContent = ""; // Clear output logs. TODO: Make it optional in the future...
-		fetch("/compile", {
-			method: 'POST',
-			headers: { 'Content-Type': 'text/plain' },
-			body: input
-		})
-		.catch(error => { output.textContent += error; })
-		.then(res => res.text())
-		.then(data => { output.textContent += data; });
-	};
+var handleClick = function() {
+	let input = edit.getValue();
+	let output = document.getElementById('output');
+	
+	output.textContent = ""; // Clear output logs. TODO: Make it optional in the future...
+	fetch("/compile", {
+		method: 'POST',
+		headers: { 'Content-Type': 'text/plain' },
+		body: input
+	})
+	.catch(error => { output.textContent += error; })
+	.then(res => res.text())
+	.then(data => { output.textContent += data; });
+};
 
-	document.addEventListener('keydown', function(evt) {
-		if (evt.ctrlKey && evt.keyCode === 13) { // CTRL + ENTER
-			evt.preventDefault();
-			handleClick();
-		}
-	});
-})();
+document.addEventListener('keydown', function(evt) {
+	if (evt.ctrlKey && evt.keyCode === 13) { // CTRL + ENTER
+		evt.preventDefault();
+		handleClick();
+	}
+});
